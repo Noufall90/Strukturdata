8fee2ce0b49e92e277f4809d89146c5542c81b9d
Noval ian <andrianau26@gmail.com>
2023-08-19 19:29:54 +0800

    tambahan

--- Tugas individu Naufal/stack.py
+++ Tugas individu Naufal/stack.py
@@ -9,10 +9,6 @@ class Stack:
         if not self.is_empty():
             return self.items.pop()
 
-    def peek(self):
-        if not self.is_empty():
-            return self.items[-1]
-
     def is_empty(self):
         return len(self.items) == 0
 
