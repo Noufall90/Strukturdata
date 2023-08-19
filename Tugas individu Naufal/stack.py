1d30eefe346a5be98dd5d94cca2ac2e3d3902db7
Noval ian <andrianau26@gmail.com>
2023-08-19 19:39:44 +0800

    queue

--- Tugas individu Naufal/stack.py
+++ Tugas individu Naufal/stack.py
@@ -23,12 +23,13 @@ for element in initial_elements:
 
 print("Angka sekarang:", stack.items)
 
-# Tambahkan angka baru
+#Tambahkan angka baru
 stack.push(6)
 stack.push(7)
 print("Angka setelah ditambah di belakang:", stack.items)
 
-# Hapus angka dari belakang
+#Hapus angka dari belakang
+#Tambahkan angka sebelah kanan dan keluarkan angka sebelah kanan juga
 out = stack.pop()
 print("Angka dihilangkan dari bagian belakang:", out)
 print("Angka hasilnya:", stack.items)
